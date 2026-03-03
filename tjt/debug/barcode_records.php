45a8a513d5335222e48df3828ab6676de8a6c09a
Chu <shengfeng2015@163.com>
2026-03-03 11:42:46 +0800

    add button

--- tjt/debug/barcode_records.php
+++ tjt/debug/barcode_records.php
@@ -450,7 +450,7 @@
         .barcode-card:nth-child(10) { animation-delay: 1.0s; }
     </style>
 </head>
-<body>
+<body> 
     <div class="container">
         <header>
             <div class="logo">
@@ -461,6 +461,22 @@
                 </div>
             </div>
         </header>
+        
+         <a href="../index.php" class="back-link">
+		   <h3 style='margin-right:800px;   display: inline-flex;
+            align-items: center;
+            padding: 8px 16px;
+            background-color: #cdd2d8 ;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            text-decoration: none;
+            color: #333;
+            font-size: 14px;
+            cursor: pointer;
+			
+			'>  <-Powrót</h1> </a>
+		
+        
         
         <div class="content">
             <div class="info-box">
