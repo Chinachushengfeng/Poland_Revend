c775ea623ce16dc42b0702fc1497ca3f66c1faeb
Chu <shengfeng2015@163.com>
2026-05-20 15:31:26 +0800

    ADS changing

---
+++ index(if you want change to your ADS ).php
@@ -0,0 +1,42 @@
+﻿ 
+ 
+  <meta charset="UTF-8">
+    <title>Black Background</title>
+    <style>
+ body{
+	 margin:0px;
+	 
+ }
+    </style>
+ 
+  
+      
+  
+      <iframe  src="tjt/index.php"  width=100% height="640"  frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="no" allowtransparency="yes" > </iframe> 
+ 
+      
+      
+	  
+	  
+	  
+	  
+	  
+	  
+	   
+
+
+
+	
+     
+
+
+
+ 
+    
+    
+   
+    
+    
+    
+    
+    
